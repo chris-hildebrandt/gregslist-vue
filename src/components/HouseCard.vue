5875cf21d41d404af5a3e96e9d777149609f9fe8
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-17 15:38:24 -0600

    modal up

---
+++ src/components/HouseCard.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="card elevation-2">
+  <div class="card-body">
+
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="modal fade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-body">
+          <!-- VVV this is what allows us to swap out the body -->
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
